f0d534ffa4fa6723ea42d1c8f2cae3f9ff4befd8
Anastasia Kapustina <nikitinaan98@mail.ru>
2023-01-25 14:17:14 +0300

    fix: webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,8 +27,18 @@ module.exports = {
         exclude: '/node_modules/'
       },
       {
-        test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
-        type: 'asset/resource'
+        test: /\.(png|svg|jpg|gif)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name].[hash][ext]'
+        }
+      },
+      {
+        test: /\.(woff(2)?|eot|ttf|otf)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'fonts/[name].[hash][ext]'
+        }
       },
       {
         test: /\.css$/,
